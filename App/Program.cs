9bce40b9a61c03da9c7823a6232b4108eebdd2dc
Gabriel Dechichi <gabrieldechichi@gmail.com>
2023-01-29 14:12:10 -0800

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+            Console.WriteLine("Test");
+        }
+
+        // static partial void HelloFrom(string name);
+    }
+}
